ddbe145a3cf264526595e24bee94ac2aef5073fb
William Kent <wjk011@gmail.com>
2021-09-24 13:48:55 -0400

    Remove Windows Forms references from GlobalUsings file
    Also renamed it to SharedGlobalUsings.cs, so individual projects
    can include their own GlobalUsings.cs file.

--- build/SharedGlobalUsings.cs
+++ build/SharedGlobalUsings.cs
@@ -2,9 +2,6 @@
 
 global using System;
 global using System.Collections.Generic;
-global using System.Drawing;
-global using System.Drawing.Drawing2D;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
-global using System.Windows.Forms;
