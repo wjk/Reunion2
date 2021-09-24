41c1f177f0f8022ce5bbdcd3cfa2234dbc67c490
William Kent <wjk011@gmail.com>
2021-09-24 13:03:57 -0400

    Initial commit of empty solution

---
+++ build/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+
+global using System;
+global using System.Collections.Generic;
+global using System.Drawing;
+global using System.Drawing.Drawing2D;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Windows.Forms;
